b304d9b7ce412d9129cfde69c4157b144167e19f
Sasha <aleksandraknott@gmail.com>
2025-02-06 21:50:52 +0000

    homework tasks

--- task.js
+++ task.js
@@ -15,3 +15,24 @@
 
 // arr.sort((a, b) => b - a);
 // alert(arr);
+
+//HOMEWORK
+//1. Using .slice() – Extract the first 3 elements from an array without modifying the original array.
+let numbers = [1, 2, 3, 4, 5];
+let firstThree = ...your code
+
+//2.Using .splice() – Remove the third element from an array and insert two new elements in its place.
+let fruits = ["apple", "banana", "cherry", "date"];
+...your code
+
+//3.Using .shift() – Remove the first element from an array and store it in a variable.
+let queue = ["first", "second", "third"];
+...your code
+
+//4.Using .unshift() – Add a new element at the beginning of an array.
+let colors = ["blue", "green"];
+...your code
+
+//5.Using .find() – Find the first number greater than 10 in an array.
+let numbers = [4, 9, 12, 15];
+let firstGreaterThanTen = ...your code
